8786fbc9d9f1167f98bd5ff44729e0ecfba8cad9
Mateusz Perczak <mateusz.perczak.mp@gmail.com>
2025-04-06 18:30:31 +0200

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: "/phbi/",
   resolve: {
     alias: {
       "@components": resolve(__dirname, "./src/components"),
